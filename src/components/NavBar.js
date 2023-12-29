60ebe6d751a9ca8993bd1011c7b0347e57a19f0f
Gyaneshwar-Seemala <johnnyseemala04@gmail.com>
2023-12-29 14:40:10 +0530

    Main Banner

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,10 +2,11 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { useEffect, useState } from 'react';
-import logo from '../assets/img/logo.svg'
-import navIcon1 from '../assets/img/nav-icon1.svg'
-import navIcon2 from '../assets/img/nav-icon2.svg'
-import navIcon3 from '../assets/img/nav-icon3.svg'
+import logo from '../assets/img/logo.svg';
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
+import '../styles/NavBar.css';
 
 export const NavBar=()=>{
     const [activeLink,setActiveLink]=useState('home');
